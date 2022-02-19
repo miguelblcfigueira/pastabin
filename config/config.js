a742f8ba5661253a49a417090fb9f95da0cbf0e8
Miguel Figueira <miguel.figueira@caixamagica.pt>
2022-02-19 18:52:43 +0000

    Use CLEARDB_DATABASE_URL in production to connect to DB.

--- config/config.js
+++ config/config.js
@@ -18,11 +18,6 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: 'CLEARDB_DATABASE_URL',
   },
 };
